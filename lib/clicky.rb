8e81761131259545d580d5f71fa1e9be4cc4bbb0
Federico Gonzalez <federico@boost.co.nz>
2011-03-25 11:31:16 +1300

    Configure everything in clicky.rb

--- lib/clicky.rb
+++ lib/clicky.rb
@@ -1,8 +1,18 @@
-require 'clicky/configuration'
-require 'clicky/log'
+require 'clicky/client'
 
 module Clicky
-  extend Configuration
   
+  mattr_accessor :endpoint
+  @@endpoint = "http://in.getclicky.com/in.php"
+  
+  mattr_accessor :site_id
+  @@site_id = nil
+  
+  mattr_accessor :sitekey_admin
+  @@sitekey_admin = nil
+  
+  def self.setup
+    yield self
+  end
   
 end
--- lib/clicky/client.rb
+++ lib/clicky/client.rb
@@ -4,44 +4,39 @@ module Clicky
   class Client
     include HTTParty
     
-    attr_accessor *Configuration::VALID_OPTIONS_KEYS
-
+    base_uri Clicky.endpoint
+    default_params :site_id => Clicky.site_id, :sitekey_admin => Clicky.sitekey_admin
+    
     def initialize(options={})
-      options = Clicky.options.merge(options)
-      Configuration::VALID_OPTIONS_KEYS.each do |key|
-        send("#{key}=", options[key])
-      end
-      
-      self.class.base_uri self.endpoint
-      self.class.default_params :site_id => self.site_id, :sitekey_admin => self.sitekey_admin
+      self.class.default_params(options)
     end
     
-    def self.page_view(request)
+    def page_view(request)
       request(:page_view, request)
     end
     
-    def self.outbound(request)
+    def outbound(request)
       request(:outbound, request)
     end
     
-    def self.download(request)
+    def download(request)
       request(:download, request)
     end
     
-    def self.click(request)
+    def click(request)
       request(:click, request)
     end
     
-    def self.request(type, request)
+    def request(type, request)
       options = {:href => request.url, 
                   :type => type.to_s, 
                   :title => title(request.path_parameters),
                   :ua => request.env["HTTP_USER_AGENT"]}
       
-      get("/", options)
+      self.class.get("/", options)
     end
     
-    def self.title(params)
+    def title(params)
       "%s/%s" % [params[:controller], params[:action]]
     end
     
